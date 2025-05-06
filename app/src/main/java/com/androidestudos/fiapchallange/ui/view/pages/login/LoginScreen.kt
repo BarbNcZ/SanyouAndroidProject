5d3b19a27a637eba487a6ccd1fecc2e2394a11b8
Barbara Niemietz <barb.niemietz@outlook.com>
2025-05-06 18:13:57 -0300

    Add: Layout adjust Login & Menu screens

--- app/src/main/java/com/androidestudos/fiapchallange/ui/view/pages/login/LoginScreen.kt
+++ app/src/main/java/com/androidestudos/fiapchallange/ui/view/pages/login/LoginScreen.kt
@@ -1,41 +1,88 @@
 package com.androidestudos.fiapchallange.ui.view.pages.login
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.androidestudos.fiapchallange.R
+import com.androidestudos.fiapchallange.ui.theme.FiapChallangePreviewTheme
 
 @Composable
 fun LoginScreen(
     login: (String, String) -> Unit
 ) {
-    Text("Email")
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .padding(start = 30.dp, end = 30.dp)
+                .align(Alignment.Center)
+        ) {
+            Image(
+                modifier = Modifier.fillMaxWidth().height(124.dp),
+                painter = painterResource(R.drawable.sanyou_logo),
+                contentDescription = null,
+            )
 
-    var email by remember {
-        mutableStateOf("")
+            var email by remember {
+                mutableStateOf("")
+            }
+
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth().padding(top = 50.dp),
+                value = email,
+                onValueChange = {email = it},
+                placeholder = {
+                    Text("email@domain.com")
+                },
+            )
+
+            var password by remember {
+                mutableStateOf("")
+            }
+
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth().padding(top = 20.dp),
+                value = password,
+                onValueChange = {password = it},
+                visualTransformation = PasswordVisualTransformation(),
+                placeholder = {
+                    Text("password")
+                },
+            )
+
+            Button(
+                modifier = Modifier.fillMaxWidth().padding(top = 20.dp),
+                onClick = {
+                    login(email, password)
+                }
+            ) {
+                Text("Continue")
+            }
+        }
     }
+}
 
-    TextField(email, {email = it})
-
-
-    Text("Password")
-
-    var password by remember {
-        mutableStateOf("")
-    }
-
-    TextField(password, {password = it}, visualTransformation = PasswordVisualTransformation())
-
-
-    Button(onClick = {
-        login(email, password)
-    }) {
-        Text("Log in")
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    FiapChallangePreviewTheme {
+        LoginScreen { _, _ ->  }
     }
 }
--- app/src/main/java/com/androidestudos/fiapchallange/ui/view/pages/menu/MenuScreen.kt
+++ app/src/main/java/com/androidestudos/fiapchallange/ui/view/pages/menu/MenuScreen.kt
@@ -1,11 +1,26 @@
 package com.androidestudos.fiapchallange.ui.view.pages.menu
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.androidestudos.fiapchallange.data.GetFuncionarioResult
-import com.androidestudos.fiapchallange.data.GetTarefasResult
-import com.androidestudos.fiapchallange.data.GetTipoTarefaResult
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.graphics.toColorInt
+import com.androidestudos.fiapchallange.R
+import com.androidestudos.fiapchallange.ui.theme.FiapChallangePreviewTheme
 
 @Composable
 fun MenuScreen(
@@ -14,28 +29,127 @@ fun MenuScreen(
     goToDeleteTaskScreen: () -> Unit,
     goToDeleteFuncionarioScreen: () -> Unit
 ) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .padding(start = 30.dp, end = 30.dp, top = 70.dp)
+                .align(Alignment.TopCenter)
+        ) {
+            Button(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally),
+                colors = ButtonColors(
+                    containerColor = Color("#EAFFDE".toColorInt()),
+                    contentColor = Color.Black,
+                    disabledContainerColor = ButtonDefaults.buttonColors().disabledContainerColor,
+                    disabledContentColor = ButtonDefaults.buttonColors().disabledContentColor,
+                ),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 15.dp
+                ),
+                border = BorderStroke(1.dp, Color.Black),
+                onClick = {
+                    goToCreateTaskScreen()
+                }
+            ) {
+                Icon(
+                    modifier = Modifier.size(25.dp).padding(end = 5.dp),
+                    painter = painterResource(R.drawable.create_task_button_icon),
+                    contentDescription = null
+                )
+                Text("Criar Novas Tarefas")
+            }
 
-    Button(onClick = {
-        goToCreateTaskScreen()
-    }) {
-        Text("Criar Tarefa")
+            Button(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 30.dp),
+                colors = ButtonColors(
+                    containerColor = Color("#FFDBDB".toColorInt()),
+                    contentColor = Color.Black,
+                    disabledContainerColor = ButtonDefaults.buttonColors().disabledContainerColor,
+                    disabledContentColor = ButtonDefaults.buttonColors().disabledContentColor,
+                ),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 15.dp
+                ),
+                border = BorderStroke(1.dp, Color.Black),
+                onClick = {
+                    goToDeleteTaskScreen()
+                }
+            ) {
+                Icon(
+                    modifier = Modifier.size(25.dp).padding(end = 5.dp),
+                    painter = painterResource(R.drawable.delete_task_button_icon),
+                    contentDescription = null
+                )
+                Text("Deletar Tarefa")
+            }
+
+            Button(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 30.dp),
+                colors = ButtonColors(
+                    containerColor = Color("#DAE0FF".toColorInt()),
+                    contentColor = Color.Black,
+                    disabledContainerColor = ButtonDefaults.buttonColors().disabledContainerColor,
+                    disabledContentColor = ButtonDefaults.buttonColors().disabledContentColor,
+                ),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 15.dp
+                ),
+                border = BorderStroke(1.dp, Color.Black),
+                onClick = {
+                    goToEmployeesScreen()
+                }
+            ) {
+                Icon(
+                    modifier = Modifier.size(25.dp).padding(end = 5.dp),
+                    painter = painterResource(R.drawable.create_employee_button_icon),
+                    contentDescription = null
+                )
+                Text("Criar Novo Funcionario")
+            }
+
+            Button(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 30.dp),
+                colors = ButtonColors(
+                    containerColor = Color("#FFDCDC".toColorInt()),
+                    contentColor = Color.Black,
+                    disabledContainerColor = ButtonDefaults.buttonColors().disabledContainerColor,
+                    disabledContentColor = ButtonDefaults.buttonColors().disabledContentColor,
+                ),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 15.dp
+                ),
+                border = BorderStroke(1.dp, Color.Black),
+                onClick = {
+                    goToDeleteFuncionarioScreen()
+                }
+            ) {
+                Icon(
+                    modifier = Modifier.size(25.dp).padding(end = 5.dp),
+                    painter = painterResource(R.drawable.delete_employee_button_icon),
+                    contentDescription = null
+                )
+                Text("Excluir Funcionario")
+            }
+        }
     }
+}
 
-    Button(onClick = {
-        goToDeleteTaskScreen()
-    }) {
-        Text("Deletar Tarefa")
-    }
-
-    Button(onClick = {
-        goToEmployeesScreen()
-    }) {
-        Text("Criar Funcionario")
-    }
-
-    Button(onClick = {
-        goToDeleteFuncionarioScreen()
-    }) {
-        Text("Deletar Funcionario")
+@Preview
+@Composable
+fun MenuScreenPreview() {
+    FiapChallangePreviewTheme {
+        MenuScreen(
+            { },
+            { },
+            { },
+            { },
+        )
     }
 }
